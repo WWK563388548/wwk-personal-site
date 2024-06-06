5b0ac9405c693b8ca344c85d8ce9ff42e45ebe6b
WWK563388548 <wc563388548@gmail.com>
2024-06-06 09:55:46 +0900

    adding header and PC nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,31 @@
+import Link from 'next/link';
+import Nav from './Nav';
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <header>Header</header>
-  )
-}
+    <header className='py-8 xl:py-12 text-white'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link href="/">
+          <h1 className='text-4xl font-semibold'>
+            WWK<span className='text-accent'>.</span>
+          </h1>
+        </Link>
+        {/* PC nav & contact me button */}
+        <div className='hidden xl:flex items-center gap-8'>
+          <Nav />
+          <Link href="/contact">
+            <Button>Contact me</Button>
+          </Link>
+        </div>
 
-export default Header
+        {/* mobile nav */}
+        <div className='xl:hidden'>
+          Mobile nav
+        </div>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
